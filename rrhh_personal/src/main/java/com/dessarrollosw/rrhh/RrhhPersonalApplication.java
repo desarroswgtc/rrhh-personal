67361323163e1cf4f9a4d01db59bd9dd54b0248d
desarroswgtc <desarrollosw@gestelcom.com>
2018-02-14 17:06:15 +0100

    first update

---
+++ rrhh_personal/src/main/java/com/dessarrollosw/rrhh/RrhhPersonalApplication.java
@@ -0,0 +1,12 @@
+package com.dessarrollosw.rrhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhPersonalApplication.class, args);
+	}
+}
---
+++ rrhh_personal/src/test/java/com/dessarrollosw/rrhh/RrhhPersonalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dessarrollosw.rrhh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RrhhPersonalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
